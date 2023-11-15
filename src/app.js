1d5c45f072c6349a37cb0ee38056fddd1de2c7ae
Alejandro Uscanga <uscanga20alex@gmail.com>
2023-11-14 21:24:27 -0500

    basice route and query

--- src/app.js
+++ src/app.js
@@ -4,10 +4,22 @@ const app = express();
 
 app.use(morgan("dev"));
 
-const sayHello = (req, res, next) => {
-  res.send("Hello!");
-};
+const sayHello = (req, res) => {
+  console.log(req.query);
+  const name = req.query.name;
+  const content = name ? `Hello, ${name}!` : "Hello!";
+  res.send(content);
+}
 
+const saySomething = (req, res) =>{
+  const greeting = req.params.greeting;
+  const name = req.query.name;
+  const content = greeting && name ? `${greeting}, ${name}!` : `${greeting}!`
+  res.send(content);
+}
+
+app.use(morgan("dev"));
 app.get("/hello", sayHello);
+app.get("/say/:greeting", saySomething);
 
 module.exports = app;
